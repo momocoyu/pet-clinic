b7aab42273930a25132e3d4710debcfc4b7cf970
Yangyang Mao <momocoyu82@gmail.com>
2019-05-08 17:04:24 -0700

    Initial project

---
+++ src/main/java/com/example/momocopetclinic/MomocoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.momocopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomocoPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomocoPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/momocopetclinic/MomocoPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.momocopetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MomocoPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
